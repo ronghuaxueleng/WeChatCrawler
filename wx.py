e15cf92564f99d882b0d6a11308e792fb8392380
caoqiang <caoqiang>
2023-11-03 15:53:12 +0800

    up

--- wx.py
+++ wx.py
@@ -1,12 +1,12 @@
-import csv
 import hashlib
 import io
-import json
 import os
 import re
 import sys
 
 import requests
+
+from db import Article, Image
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')
 url_file = "url1.csv"
@@ -16,21 +16,27 @@ name = ['title', 'link', 'create_time']
 def save_image_urls():
     pattern = r"data-src=\"(?P<url>https:\/\/mmbiz\.qpic\.cn\S+)\""
     regex = re.compile(pattern)
-    all_urls = []
-    with open(url_file, "r") as csvfile:
-        csvreader = csv.reader(csvfile)
-        next(csvreader)
-        for row in csvreader:
-            url = row[1]
+    articles = Article.select().where(Article.grabState != 1).execute()
+    if len(articles) > 0:
+        for article in articles:
+            url = article.url
             print(url)
             res = requests.get(url)
             text = res.text
-            urls = regex.findall(text)
-            if len(urls) > 0:
-                all_urls.extend(urls)
-        with open('data.json', 'w') as f1:
-            # 使用json.dump()函数将序列化后的JSON格式的数据写入到文件中
-            json.dump(all_urls, f1, ensure_ascii=False)
+            image_urls = regex.findall(text)
+            md = hashlib.md5()
+            md.update(url.encode('utf-8'))
+            postId = md.hexdigest()
+            if len(image_urls) > 0:
+                for image_url in image_urls:
+                    try:
+                        image_data = {}
+                        image_data['postId'] = postId
+                        image_data['image_url'] = image_url
+                        Image.insert(image_data).execute()
+                    except Exception as e:
+                        print(e)
+            Article.update(grabState=1).where(Article.postId == postId).execute()
 
 
 def request_get(url, ret_type="text", timeout=5, encoding="GBK"):
@@ -57,9 +63,9 @@ def save_image(image_src):
 
 
 if __name__ == '__main__':
-    # save_image_urls()
-    with open('data.json', 'r', encoding='UTF-8') as f:
-        os.makedirs('img', exist_ok=True)
-        data_list = json.load(f)
-        for image_src in data_list:
-            save_image(image_src)
+    save_image_urls()
+    # with open('data.json', 'r', encoding='UTF-8') as f:
+    #     os.makedirs('img', exist_ok=True)
+    #     data_list = json.load(f)
+    #     for image_src in data_list:
+    #         save_image(image_src)
