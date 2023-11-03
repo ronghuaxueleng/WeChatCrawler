da6a7c5cc0a7e1d31e3c8944d29bd6867f8cd086
caoqiang <caoqiang>
2023-11-03 10:46:38 +0800

    up

--- WeixinSpider.py
+++ WeixinSpider.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
-import requests
-from lxml import etree
-import json
 import re
+
 import pandas as pd
 import pymysql
+import requests
+from lxml import etree
 
 
 class WeixinSpider:
@@ -13,20 +13,19 @@ class WeixinSpider:
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"}
 
-
     def parse_url(self, url):  # 发送请求，获取响应
         print(url)
         response = requests.get(url, headers=self.headers)
         return response.content.decode()
-    
-    def get_content_list(self,html_str):#提取数据
+
+    def get_content_list(self, html_str):  # 提取数据
         html = etree.HTML(html_str)
         content_list = []
         item = {}
         item["title"] = html.xpath("//*[@id=\"activity-name\"]/text()")
-#         item["title"] = [i.replace("\n","").replace(" ","") for i in item["title"]]
+        #         item["title"] = [i.replace("\n","").replace(" ","") for i in item["title"]]
         item["laiyuan"] = html.xpath("//*[@id=\"js_name\"]/text()")
-#         item["laiyuan"] = [i.replace("\n","").replace(" ","") for i in item["laiyuan"]]
+        #         item["laiyuan"] = [i.replace("\n","").replace(" ","") for i in item["laiyuan"]]
         item["other"] = html.xpath("//*[@id=\"js_content\"]//text()")
         print(item)
         content_list.append(item)
@@ -35,30 +34,30 @@ class WeixinSpider:
 
     def save_html(self, html_str, page_name):  # 保存html字符串
         file_path = "it/{}.html".format(page_name)
-        with open(file_path, "w", encoding="utf-8") as f: 
+        with open(file_path, "w", encoding="utf-8") as f:
             f.write(html_str)
 
     def get_url_list(self):
         file_path = "it.csv"
         df = pd.read_csv(file_path)
-        temp_list = df["link"].str.split("!").tolist() #[[],[],[]]
+        temp_list = df["link"].str.split("!").tolist()  # [[],[],[]]
         num_list = list(set([i for j in temp_list for i in j]))
         num_list_new = [i for i in num_list]
-        
-        time_list = df["create_time"].tolist() #[[],[],[]]
-        return num_list_new,time_list
+
+        time_list = df["create_time"].tolist()  # [[],[],[]]
+        return num_list_new, time_list
 
     def run(self):  # 实现主要逻辑
         # 1.构造url列表
         # 2.遍历，发送请求，获取响应
-        url_list,time_list = self.get_url_list()
-    
+        url_list, time_list = self.get_url_list()
+
         # 打开数据库连接（ip/数据库用户名/登录密码/数据库名）
         db = pymysql.connect("localhost", "root", "root", "weixin_database")
-    
+
         # 使用 cursor() 方法创建一个游标对象 cursor
         cursor = db.cursor()
-   
+
         for url in url_list:
             num = url_list.index(url)
             print(num)
@@ -68,31 +67,30 @@ class WeixinSpider:
             laiyuan = ''.join(content_list[0]["laiyuan"])
             other = '\n'.join(content_list[0]["other"])
             create_time = time_list[num]
-            p = re.compile('<div class="rich_media_content " id="js_content">.*?</div>',re.S)
-            html = re.search(p,html_str)
-            if(html):
-                html = re.search(p3,html_str).group().replace("\n","")
-                
+            p = re.compile('<div class="rich_media_content " id="js_content">.*?</div>', re.S)
+            html = re.search(p, html_str)
+            if (html):
+                html = re.search(p3, html_str).group().replace("\n", "")
+
             else:
-                html = html_str.replace("\n","")
+                html = html_str.replace("\n", "")
             sql = """INSERT INTO weixin_table(title,url,other,html,create_time,type_id)
-                VALUES ({},{},{},{},{},{},{},{})""".format('"'+title+'"','"'+url+'"','"'+other+'"',"'"+html+"'",create_time,1)
+                VALUES ({},{},{},{},{},{},{},{})""".format('"' + title + '"', '"' + url + '"', '"' + other + '"',
+                                                           "'" + html + "'", create_time, 1)
             try:
-               # 执行sql语句
+                # 执行sql语句
                 cursor.execute(sql)
-               # 提交到数据库执行
+                # 提交到数据库执行
                 db.commit()
             except:
-                print("第"+num+"条数据插入失败")
-               # 如果发生错误则回滚
+                print("第" + num + "条数据插入失败")
+                # 如果发生错误则回滚
                 db.rollback()
-               
 
-            
             # 3.保存html
             page_name = title
-            self.save_html(html_str, page_name)      
-        # 关闭数据库连接
+            self.save_html(html_str, page_name)
+            # 关闭数据库连接
         db.close()
 
 
